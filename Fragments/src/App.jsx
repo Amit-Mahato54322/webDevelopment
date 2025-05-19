67f2f2ee671de187fc1dd5a2cd6e8570cfc3213b
Amit-Mahato54322 <amitmahato54322@gmail.com>
2025-05-19 15:30:17 -0500

    first commit

---
+++ Fragments/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  let foodItems = ["Dal", "Chicken", "Potato", "Roti", "Salad", "Milk"]
+
+  return (
+    <>  {/* This part is known as fragments*/}
+
+    </>
+  )
+}
+
+export default App
