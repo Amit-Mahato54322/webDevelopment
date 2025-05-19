c42f441e0fac85f148f68fbf01e2b8da590f1f4c
Amit-Mahato54322 <amitmahato54322@gmail.com>
2025-05-19 16:01:25 -0500

    create an unordered list

--- Fragments/src/App.jsx
+++ Fragments/src/App.jsx
@@ -1,12 +1,18 @@
+import React from 'react'
 import { useState } from 'react'
-
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   let foodItems = ["Dal", "Chicken", "Potato", "Roti", "Salad", "Milk"]
 
   return (
     <>  {/* This part is known as fragments*/}
-
+      <h1>Healthy Food</h1>
+      <ul className='list-group'>
+        {foodItems.map(
+          item => <li key={item  } className='list-group-item'>{item}</li>
+          )}
+      </ul>
     </>
   )
 }
