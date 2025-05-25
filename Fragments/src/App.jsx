f13831178ee2ed8a2a218fafdac622d619dc9b59
Amit-Mahato54322 <amitmahato54322@gmail.com>
2025-05-24 22:25:08 -0500

    initial commit'

--- Fragments/src/App.jsx
+++ Fragments/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { useState } from 'react'
 import "bootstrap/dist/css/bootstrap.min.css";
 
+
 function App() {
   let foodItems = ["Dal", "Chicken", "Potato", "Roti", "Salad", "Milk"]
-
+  
   return (
     <>  {/* This part is known as fragments*/}
       <h1>Healthy Food</h1>
+      {emptyMessage}
       <ul className='list-group'>
         {foodItems.map(
           item => <li key={item  } className='list-group-item'>{item}</li>
