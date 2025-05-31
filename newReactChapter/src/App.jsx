1a799130fa6d71cd1b414a48ca1bc958d860190e
Amit-Mahato54322 <amitmahato54322@gmail.com>
2025-05-31 11:36:58 -0500

    used first conditional rendering method

--- newReactChapter/src/App.jsx
+++ newReactChapter/src/App.jsx
@@ -3,10 +3,12 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import './App.css'
 
 function App(){
-  let foodList = ['Apple', 'Mango', 'Orange']
+  let foodList = []
+  let emptyMessage = foodList.length == 0? <h3>There's no food</h3>:null
   return(<>
    <div>
      <ul className="list-group">
+      {emptyMessage}
       {/* I was using a curly braces and not returning anything, which was causing the main issue, so i changed it to () to implicitly return the jsx */}
       {foodList.map((item) =>(
         <li key = {item} className="list-group-item">{
